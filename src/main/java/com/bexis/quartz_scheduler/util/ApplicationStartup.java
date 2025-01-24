6b4547979998205aaa98c8d912b89c2a97ceb8b2
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-24 20:59:46 +0000

    schedule tasks of startup

---
+++ src/main/java/com/bexis/quartz_scheduler/util/ApplicationStartup.java
@@ -0,0 +1,34 @@
+package com.bexis.quartz_scheduler.util;
+
+import com.bexis.quartz_scheduler.model.Task;
+import com.bexis.quartz_scheduler.repository.TaskRepository;
+import com.bexis.quartz_scheduler.service.TaskSchedulerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationStartup {
+    private final TaskRepository taskRepository;
+    private final TaskSchedulerService taskSchedulerService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        List<Task> tasks = taskRepository.findAll();
+
+        for (Task task : tasks) {
+            if (task.getHasSentDeadlineNotification() == 0) {
+                try {
+                    taskSchedulerService.scheduleTask(task);
+                    System.out.println("On start up scheduling, taskId: " + task.getTaskId());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
