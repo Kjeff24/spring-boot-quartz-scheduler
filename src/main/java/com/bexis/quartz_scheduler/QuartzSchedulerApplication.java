95eb9bc9d2ad189bd8bd031110ac2c15e1b62912
Kjeff24 <jeffreyarthur123@gmail.com>
2025-01-24 20:59:29 +0000

    Created a mock data on startup

--- src/main/java/com/bexis/quartz_scheduler/QuartzSchedulerApplication.java
+++ src/main/java/com/bexis/quartz_scheduler/QuartzSchedulerApplication.java
@@ -1,13 +1,46 @@
 package com.bexis.quartz_scheduler;
 
+import com.bexis.quartz_scheduler.model.Task;
+import com.bexis.quartz_scheduler.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class QuartzSchedulerApplication {
+import java.time.LocalDateTime;
+import java.util.List;
 
+@SpringBootApplication
+@RequiredArgsConstructor
+public class QuartzSchedulerApplication implements CommandLineRunner {
+	private final TaskRepository taskRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(QuartzSchedulerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Task task1 = Task.builder()
+				.name("Sleep time")
+				.assignedTo("user1")
+				.deadline(LocalDateTime.now().plusMinutes(10))
+				.description("See to it you sleep")
+				.responsibility("You are to sleep by then")
+				.status("open")
+				.createdBy("admin")
+				.build();
+
+		Task task2 = Task.builder()
+				.name("Wake up time")
+				.assignedTo("user1")
+				.deadline(LocalDateTime.now().plusMinutes(15))
+				.description("Alarm time")
+				.responsibility("You are to wake up by then")
+				.status("open")
+				.createdBy("admin")
+				.build();
+
+		taskRepository.saveAll(List.of(task1, task2));
 	}
 
 }
